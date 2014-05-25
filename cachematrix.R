e6505b4230b343c42dcb5fde6c8a643d7b82cff1
amnovoao <amnovoao@gmail.com>
2014-05-25 15:50:43 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function uses solve() to find inverse of a special 
+## matrix object and cache it using a free floating variable.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    a<-NULL
+    set<-function(y){
+        x<<-y
+        a<<-NULL
+    }
+    get<-function() x
+    setmatrix<-function(solve) a<<- solve
+    getmatrix<-function() a
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special matrix returned by the function makeCacheMatrix above.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+    a<-x$getmatrix()
+    if(!is.null(a)){
+        message("getting cached data")
+        return(a)
+    }
+    matrix<-x$get()
+    a<-solve(matrix, ...)
+    x$setmatrix(a)
+    a
 }
+
+
+##Execute each of this lines to probe the function (2X2 Matrix)
+
+a<-makeCacheMatrix()
+a$set(matrix(1:4,2,2)) ## Here you can change the values of each element of the matrix
+cacheSolve(a)
+
+
+##Execute each of this lines to probe the function (3X3 Matrix)
+
+z<-makeCacheMatrix()
+z$set(matrix(c(2,5,1,3),2,2)) ## Here you can change the values of each element of the matrix
+cacheSolve(z)
